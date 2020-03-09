6b1541821eebc01377dd3c17301d763c590674c1
Mathieu Barnachon <mathieu@styleshoots.com>
2020-03-09 17:10:23 +0100

    Use the temporary version number.

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
     ],
     dependencies: [
         //.package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", from: "3.1.0"),
-        .package(url: "https://github.com/mbarnach/Swift-Kuery.git", .branch("SQL-COUNT")),
+        .package(url: "https://github.com/mbarnach/Swift-Kuery.git", from: "3.0.2"),
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
